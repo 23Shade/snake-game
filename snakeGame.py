1a0a32af0fe31dac346b8a0460e3684b5b76fd3c
Shade <93147501+23Shade@users.noreply.github.com>
2023-04-06 15:31:20 +0800

    Converted the code into snake_case

--- snakeGame.py
+++ snakeGame.py
@@ -8,9 +8,9 @@ import sys
 pygame.init()
 
 # Game window setup
-windowWidth = 600
-windowHeight = 600
-windowScreen = pygame.display.set_mode((windowWidth, windowHeight))
+window_width = 600
+window_height = 600
+window_screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Snake Game by Shade')
 
 # Colors
@@ -25,49 +25,49 @@ fps = pygame.time.Clock()
  
 # Game Elements
 # Snake body (first 3 blocks)
-snakeBody = [[100, 50], [90, 50], [80, 50]]
+snake_body = [[100, 50], [90, 50], [80, 50]]
 # Default snake position
-snakePosition = [100, 50]
+snake_position = [100, 50]
 # Increase/Decrease the number of px per movement
-snakeMovement = 10
+snake_movement = 10
 # Default snake direction
 direction = 'RIGHT'
-nextDirection = direction
+next_direction = direction
 # Snake speed
 difficulty = 20
 # Fruit spawn
-fruitSpawn = True
+fruit_spawn = True
 # fruit position
-fruitPosition = [random.randrange(1, (windowWidth//10)) * 10,
-				random.randrange(1, (windowHeight//10)) * 10]
+fruit_position = [random.randrange(1, (window_width//10)) * 10,
+				random.randrange(1, (window_height//10)) * 10]
 # Initial Score
 score = 0
 
 # Load menu images
-menuBackground = pygame.image.load('res/MenuBackground.png')
-menuApples = pygame.image.load('res/MenuApples.png')
-menuClouds = pygame.image.load('res/MenuClouds.png')
-menuEnter = pygame.image.load('res/MenuEnter.png')
-menuTitle = pygame.image.load('res/MenuTitle.png')
+menu_background = pygame.image.load('res/MenuBackground.png')
+menu_apples = pygame.image.load('res/MenuApples.png')
+menu_clouds = pygame.image.load('res/MenuClouds.png')
+menu_enter = pygame.image.load('res/MenuEnter.png')
+menu_title = pygame.image.load('res/MenuTitle.png')
 
 # Scale down the menu images
 # 951 x 951 px original size
-scaleFactor = min(windowWidth / 951, windowHeight / 951)
+menu_scale_factor = min(window_width / 951, window_height / 951)
 
-menuBackground = pygame.transform.scale(menuBackground, (windowWidth, windowHeight))
-menuApples = pygame.transform.scale(menuApples, (int(951 * scaleFactor), int(951 * scaleFactor)))
-menuClouds = pygame.transform.scale(menuClouds, (int(951 * scaleFactor), int(951 * scaleFactor)))
-menuEnter = pygame.transform.scale(menuEnter, (int(951 * scaleFactor), int(951 * scaleFactor)))
-menuTitle = pygame.transform.scale(menuTitle, (int(951 * scaleFactor), int(951 * scaleFactor)))
+menu_background = pygame.transform.scale(menu_background, (window_width, window_height))
+menu_apples = pygame.transform.scale(menu_apples, (int(951 * menu_scale_factor), int(951 * menu_scale_factor)))
+menu_clouds = pygame.transform.scale(menu_clouds, (int(951 * menu_scale_factor), int(951 * menu_scale_factor)))
+menu_enter = pygame.transform.scale(menu_enter, (int(951 * menu_scale_factor), int(951 * menu_scale_factor)))
+menu_title = pygame.transform.scale(menu_title, (int(951 * menu_scale_factor), int(951 * menu_scale_factor)))
 
 # Center the images on the screen
-menuApplesRect = menuApples.get_rect(center=(windowWidth//2, windowHeight//2))
-menuCloudsRect = menuClouds.get_rect(center=(windowWidth//2, windowHeight//2))
-menuEnterRect = menuEnter.get_rect(center=(windowWidth//2, windowHeight//2))
-menuTitleRect = menuTitle.get_rect(center=(windowWidth//2, windowHeight//2))
+menu_apples_rect = menu_apples.get_rect(center=(window_width//2, window_height//2))
+menu_clouds_rect = menu_clouds.get_rect(center=(window_width//2, window_height//2))
+menu_enter_rect = menu_enter.get_rect(center=(window_width//2, window_height//2))
+menu_title_rect = menu_title.get_rect(center=(window_width//2, window_height//2))
 
 # Main menu function
-def mainMenu():
+def main_menu():
     # Load the main menu background song
     main_menu_sound = pygame.mixer.Sound('res/MainMenuSound.mp3')
     # Play the main menu background song in an infinite loop
@@ -93,13 +93,13 @@ def mainMenu():
                 sys.exit()
 
         # Fill the screen with black color
-        windowScreen.fill(black)
+        window_screen.fill(black)
 
         # Draw the menu images
-        windowScreen.blit(menuBackground, (0, 0))
-        windowScreen.blit(menuApples, menuApplesRect)
-        windowScreen.blit(menuClouds, menuCloudsRect)
-        windowScreen.blit(menuTitle, menuTitleRect)
+        window_screen.blit(menu_background, (0, 0))
+        window_screen.blit(menu_apples, menu_apples_rect)
+        window_screen.blit(menu_clouds, menu_clouds_rect)
+        window_screen.blit(menu_title, menu_title_rect)
 
         # Adjust the MenuEnter transparency value
         alpha += alpha_change_rate
@@ -107,8 +107,8 @@ def mainMenu():
             alpha_change_rate = -alpha_change_rate
 
         # Set the MenuEnter transparency value
-        menuEnter.set_alpha(alpha)
-        windowScreen.blit(menuEnter, menuEnterRect)
+        menu_enter.set_alpha(alpha)
+        window_screen.blit(menu_enter, menu_enter_rect)
 
         # Update the display
         pygame.display.update()
@@ -117,51 +117,51 @@ def mainMenu():
         fps.tick(30)
 
 # Call the main menu function
-mainMenu()
+main_menu()
 # When exiting the main menu, stop the background song
 pygame.mixer.stop()
 
 # Score function
-def totalScore(choice, color, font, size):
+def total_score(choice, color, font, size):
     # Creates a font object
-    scoreFont = pygame.font.SysFont(font, size)
+    score_font = pygame.font.SysFont(font, size)
     # Live score
-    scoreSurface = scoreFont.render('Score : ' + str(score), True, color)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
     # Rectangle object
-    scoreRect = scoreSurface.get_rect()
+    scoreRect = score_surface.get_rect()
     # Score surface and rectangle will be drawn
-    windowScreen.blit(scoreSurface, scoreRect)
+    window_screen.blit(score_surface, scoreRect)
 
 # Game Over function
-def gameOver():
+def game_over():
     # Load GameOver image
-    gameOverImg = pygame.image.load('res/GameOver.png')
+    game_over_img = pygame.image.load('res/GameOver.png')
     # Scale down the image
-    gameOverImg = pygame.transform.scale(gameOverImg, (324.5, 191.5))
+    game_over_img = pygame.transform.scale(game_over_img, (324.5, 191.5))
     # Get the rect for the image
-    gameOverImgRect = gameOverImg.get_rect()
+    game_over_img_rect = game_over_img.get_rect()
     # Set the position of the image in the middle of the screen
-    gameOverImgRect.center = (windowWidth/2, windowHeight/2)
+    game_over_img_rect.center = (window_width/2, window_height/2)
     # Create a font object for the score
-    finalScoreFont = pygame.font.SysFont('times new roman', 20)
+    final_score_font = pygame.font.SysFont('times new roman', 20)
     # Render the score text
-    finalScoreSurface = finalScoreFont.render('score: ' + str(score), True, white)
+    final_score_surface = final_score_font.render('score: ' + str(score), True, white)
     # Get the rect for the score text
-    finalScoreRect = finalScoreSurface.get_rect()
+    final_score_rect = final_score_surface.get_rect()
     # Set the position of the score text below the image
-    finalScoreRect.midtop = (windowWidth/2, gameOverImgRect.bottom + 20)
+    final_score_rect.midtop = (window_width/2, game_over_img_rect.bottom + 20)
     # Fill the screen with black color
-    windowScreen.fill(black)
+    window_screen.fill(black)
     # Draw the image on the screen
-    windowScreen.blit(gameOverImg, gameOverImgRect)
+    window_screen.blit(game_over_img, game_over_img_rect)
     # Draw the score text on the screen
-    windowScreen.blit(finalScoreSurface, finalScoreRect)
+    window_screen.blit(final_score_surface, final_score_rect)
     # Update the display
     pygame.display.flip()
     # Load the sound file
-    gameOverSound = pygame.mixer.Sound('res/GameOverSound.mp3')
+    game_over_sound = pygame.mixer.Sound('res/GameOverSound.mp3')
     # Play the sound
-    gameOverSound.play()
+    game_over_sound.play()
     # Print the score
     print("Score:", score)
     # Wait for 3 seconds before quitting
@@ -171,116 +171,116 @@ def gameOver():
     sys.exit()
 
 # Key events function
-def keyEvents():
-    global nextDirection
+def key_events():
+    global next_direction
     # Arrow keys and WASD keys
     # ESC to exit
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key in (pygame.K_UP, pygame.K_w):
-                nextDirection = 'UP'
+                next_direction = 'UP'
             elif event.key in (pygame.K_DOWN, pygame.K_s):
-                nextDirection = 'DOWN'
+                next_direction = 'DOWN'
             elif event.key in (pygame.K_LEFT, pygame.K_a):
-                nextDirection = 'LEFT'
+                next_direction = 'LEFT'
             elif event.key in (pygame.K_RIGHT, pygame.K_d):
-                nextDirection = 'RIGHT'
+                next_direction = 'RIGHT'
             elif event.key in (pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
 
 # Direction management function
-def manageDirection():
-    global direction, nextDirection
-    if nextDirection == 'UP' and direction != 'DOWN':
+def manage_direction():
+    global direction, next_direction
+    if next_direction == 'UP' and direction != 'DOWN':
         direction = 'UP'
-    elif nextDirection == 'DOWN' and direction != 'UP':
+    elif next_direction == 'DOWN' and direction != 'UP':
         direction = 'DOWN'
-    elif nextDirection == 'LEFT' and direction != 'RIGHT':
+    elif next_direction == 'LEFT' and direction != 'RIGHT':
         direction = 'LEFT'
-    elif nextDirection == 'RIGHT' and direction != 'LEFT':
+    elif next_direction == 'RIGHT' and direction != 'LEFT':
         direction = 'RIGHT'
 
 # Snake movement function
-def moveSnake():
-    global direction, snakePosition, snakeBody, fruitPosition, fruitSpawn, score
-    # snakeMovement = 10
+def move_snake():
+    global direction, snake_position, snake_body, fruit_position, fruit_spawn, score
+    # snake_movement = 10
     # Y - coordinate decreases by 10px
     if direction == 'UP':
-        snakePosition[1] -= snakeMovement
+        snake_position[1] -= snake_movement
     # Y - coordinate increases by 10px
     elif direction == 'DOWN':
-        snakePosition[1] += snakeMovement
+        snake_position[1] += snake_movement
     # X - coordinate decreases by 10px
     elif direction == 'LEFT':
-        snakePosition[0] -= snakeMovement
+        snake_position[0] -= snake_movement
     # X - coordinate increases by 10px
     elif direction == 'RIGHT':
-        snakePosition[0] += snakeMovement
+        snake_position[0] += snake_movement
 
     # Snake body growing mechanism
 	# if fruits and snakes collide then scores
 	# will be incremented by 10
-    snakeBody.insert(0, list(snakePosition))
-    if (abs(snakePosition[0] - fruitPosition[0]) < 10) and (abs(snakePosition[1] - fruitPosition[1]) < 10):
+    snake_body.insert(0, list(snake_position))
+    if (abs(snake_position[0] - fruit_position[0]) < 10) and (abs(snake_position[1] - fruit_position[1]) < 10):
         score += 1
-        fruitSpawn = False
+        fruit_spawn = False
         # Load the sound file
-        gameOverSound = pygame.mixer.Sound('res/EatSound.mp3')
+        game_over_sound = pygame.mixer.Sound('res/EatSound.mp3')
         # Play the sound
-        gameOverSound.play()
+        game_over_sound.play()
     else:
-        snakeBody.pop()
+        snake_body.pop()
 
 # Fruit spawn function
-def spawnFruit():
-    global fruitPosition, fruitSpawn, snakeBody
+def spawn_fruit():
+    global fruit_position, fruit_spawn, snake_body
     # Finds a valid spawn for the fruit and prevents the fruit to spawn on the snake's body	
-    if not fruitSpawn:
-        fruitPosition = [random.randrange(1, (windowWidth//10)) * 10,
-                        random.randrange(1, (windowHeight//10)) * 10]
-        while fruitPosition in snakeBody:
-            fruitPosition = [random.randrange(1, (windowWidth//10)) * 10,
-                            random.randrange(1, (windowHeight//10)) * 10]
-    fruitSpawn = True
+    if not fruit_spawn:
+        fruit_position = [random.randrange(1, (window_width//10)) * 10,
+                        random.randrange(1, (window_height//10)) * 10]
+        while fruit_position in snake_body:
+            fruit_position = [random.randrange(1, (window_width//10)) * 10,
+                            random.randrange(1, (window_height//10)) * 10]
+    fruit_spawn = True
 
 # Object function
-def drawObject():
-    windowScreen.fill(black)
-    for pos in snakeBody:
-        pygame.draw.rect(windowScreen, pink, pygame.Rect(pos[0], pos[1], 10, 10))
+def draw_object():
+    window_screen.fill(black)
+    for pos in snake_body:
+        pygame.draw.rect(window_screen, pink, pygame.Rect(pos[0], pos[1], 10, 10))
         # Load apple image with alpha channel
-        appleImg = pygame.image.load('res/Apple.png').convert_alpha() 
+        apple_img = pygame.image.load('res/Apple.png').convert_alpha() 
         # Scale down the apple image to 10x10 pixels
-        appleImg = pygame.transform.scale(appleImg, (10, 10)) 
+        apple_img = pygame.transform.scale(apple_img, (10, 10)) 
         # Draw the apple image on the screen
-        windowScreen.blit(appleImg, (fruitPosition[0], fruitPosition[1])) 
+        window_screen.blit(apple_img, (fruit_position[0], fruit_position[1])) 
         # Displays the score countinuously
-        totalScore(1, white, 'times new roman', 20)
+        total_score(1, white, 'times new roman', 20)
 
 # Game Over conditions function
-def checkGameOver():
+def check_game_over():
     # Touching the edge of the screen (Wall Collision)
-    if snakePosition[0] < 0 or snakePosition[0] > windowWidth-10:
-        gameOver()
-    elif snakePosition[1] < 0 or snakePosition[1] > windowWidth-10:
-        gameOver()
+    if snake_position[0] < 0 or snake_position[0] > window_width-10:
+        game_over()
+    elif snake_position[1] < 0 or snake_position[1] > window_width-10:
+        game_over()
 
     # Touching the snake body (Body Collision)
-    # First block/Snake's head = snakeBody[1:]
-    for block in snakeBody[1:]:
-        if snakePosition[0] == block[0] and snakePosition[1] == block[1]:
-            gameOver()
+    # First block/Snake's head = snake_body[1:]
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            game_over()
 
 # Main function
 def main():
     while True:
-        keyEvents()
-        manageDirection()
-        moveSnake()
-        spawnFruit()
-        drawObject()
-        checkGameOver()
+        key_events()
+        manage_direction()
+        move_snake()
+        spawn_fruit()
+        draw_object()
+        check_game_over()
         # Updates the entire pygame display
         pygame.display.update()
         # Maximum FPS allowed
