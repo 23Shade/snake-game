9c0a458cdb0086302289c142af42429278704153
Shade <93147501+23Shade@users.noreply.github.com>
2023-04-08 11:21:53 +0800

    Added more game over elements

--- snakeGame.py
+++ snakeGame.py
@@ -8,8 +8,8 @@ import sys
 pygame.init()
 
 # Game window setup
-window_width = 600
-window_height = 600
+window_width = 500
+window_height = 500
 window_screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Snake Game by Shade')
 
@@ -43,14 +43,14 @@ fruit_position = [random.randrange(1, (window_width//10)) * 10,
 # Initial Score
 score = 0
 
-# Load menu images
+# Load the main menu images
 menu_background = pygame.image.load('res/MenuBackground.png')
 menu_apples = pygame.image.load('res/MenuApples.png')
 menu_clouds = pygame.image.load('res/MenuClouds.png')
 menu_enter = pygame.image.load('res/MenuEnter.png')
 menu_title = pygame.image.load('res/MenuTitle.png')
 
-# Scale down the menu images
+# Scale down the main menu images
 # 951 x 951 px original size
 menu_scale_factor = min(window_width / 951, window_height / 951)
 
@@ -60,11 +60,29 @@ menu_clouds = pygame.transform.scale(menu_clouds, (int(951 * menu_scale_factor),
 menu_enter = pygame.transform.scale(menu_enter, (int(951 * menu_scale_factor), int(951 * menu_scale_factor)))
 menu_title = pygame.transform.scale(menu_title, (int(951 * menu_scale_factor), int(951 * menu_scale_factor)))
 
-# Center the images on the screen
+# Center the main menu images on the screen
 menu_apples_rect = menu_apples.get_rect(center=(window_width//2, window_height//2))
 menu_clouds_rect = menu_clouds.get_rect(center=(window_width//2, window_height//2))
 menu_enter_rect = menu_enter.get_rect(center=(window_width//2, window_height//2))
 menu_title_rect = menu_title.get_rect(center=(window_width//2, window_height//2))
+
+# Load the game over images
+game_over_background = pygame.image.load('res/GameOverBackground.png')
+game_over_apples = pygame.image.load('res/GameOverApples.png')
+game_over_text = pygame.image.load('res/GameOver.png')
+
+# Scale down the game over images
+# 951 x 951 px original size
+game_over_scale_factor = min(window_width / 951, window_height / 951)
+
+game_over_background = pygame.transform.scale(game_over_background, (window_width, window_height))
+game_over_text = pygame.transform.scale(game_over_text, (int(951 * game_over_scale_factor), int(951 * game_over_scale_factor)))
+game_over_apples = pygame.transform.scale(game_over_apples, (int(951 * game_over_scale_factor), int(951 * game_over_scale_factor)))
+
+# Center the game over images on the screen
+game_over_background_rect = game_over_background.get_rect(center=(window_width//2, window_height//2))
+game_over_text_rect = game_over_text.get_rect(center=(window_width//2, window_height//2))
+game_over_apples_rect = game_over_apples.get_rect(center=(window_width//2, window_height//2))
 
 # Main menu function
 def main_menu():
@@ -134,36 +152,23 @@ def total_score(choice, color, font, size):
 
 # Game Over function
 def game_over():
-    # Load GameOver image
-    game_over_img = pygame.image.load('res/GameOver.png')
-    # Scale down the image
-    game_over_img = pygame.transform.scale(game_over_img, (324.5, 191.5))
-    # Get the rect for the image
-    game_over_img_rect = game_over_img.get_rect()
-    # Set the position of the image in the middle of the screen
-    game_over_img_rect.center = (window_width/2, window_height/2)
-    # Create a font object for the score
-    final_score_font = pygame.font.SysFont('times new roman', 20)
+    # Draw the game over images
+    window_screen.blit(game_over_background, (0, 0))
+    window_screen.blit(game_over_text, game_over_text_rect)
+    window_screen.blit(game_over_apples, game_over_apples_rect)
     # Render the score text
-    final_score_surface = final_score_font.render('score: ' + str(score), True, white)
-    # Get the rect for the score text
-    final_score_rect = final_score_surface.get_rect()
-    # Set the position of the score text below the image
-    final_score_rect.midtop = (window_width/2, game_over_img_rect.bottom + 20)
-    # Fill the screen with black color
-    window_screen.fill(black)
-    # Draw the image on the screen
-    window_screen.blit(game_over_img, game_over_img_rect)
-    # Draw the score text on the screen
-    window_screen.blit(final_score_surface, final_score_rect)
-    # Update the display
-    pygame.display.flip()
+    font = pygame.font.SysFont(None, 20)
+    text = font.render(f"Score: {score}", True, white)
+    text_rect = text.get_rect(center=(window_width//2, window_height//2 + 100))
+    window_screen.blit(text, text_rect)
     # Load the sound file
     game_over_sound = pygame.mixer.Sound('res/GameOverSound.mp3')
     # Play the sound
     game_over_sound.play()
     # Print the score
     print("Score:", score)
+    # Update the display
+    pygame.display.update()
     # Wait for 3 seconds before quitting
     time.sleep(3)
     # Quit pygame and terminate the script
